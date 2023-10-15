be43abff5e5b19cea20ac0215bb40d1dce10067c
Alik <moisesalik01@gmail.com>
2023-10-15 16:18:10 -0500

    problema 6 terminado

--- FrmProb6.Designer.vb
+++ FrmProb6.Designer.vb
@@ -22,9 +22,129 @@ Partial Class FrmProb6
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container
+        Me.ButtonExplorar = New System.Windows.Forms.Button()
+        Me.TextBoxMas = New System.Windows.Forms.TextBox()
+        Me.TextBoxMenos = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.ButtonGuardado = New System.Windows.Forms.Button()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'ButtonExplorar
+        '
+        Me.ButtonExplorar.Location = New System.Drawing.Point(109, 129)
+        Me.ButtonExplorar.Name = "ButtonExplorar"
+        Me.ButtonExplorar.Size = New System.Drawing.Size(193, 100)
+        Me.ButtonExplorar.TabIndex = 0
+        Me.ButtonExplorar.Text = "Explorador de archivos"
+        Me.ButtonExplorar.UseVisualStyleBackColor = True
+        '
+        'TextBoxMas
+        '
+        Me.TextBoxMas.Location = New System.Drawing.Point(52, 346)
+        Me.TextBoxMas.Multiline = True
+        Me.TextBoxMas.Name = "TextBoxMas"
+        Me.TextBoxMas.Size = New System.Drawing.Size(250, 53)
+        Me.TextBoxMas.TabIndex = 1
+        '
+        'TextBoxMenos
+        '
+        Me.TextBoxMenos.Location = New System.Drawing.Point(499, 346)
+        Me.TextBoxMenos.Multiline = True
+        Me.TextBoxMenos.Name = "TextBoxMenos"
+        Me.TextBoxMenos.Size = New System.Drawing.Size(289, 53)
+        Me.TextBoxMenos.TabIndex = 2
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.0!)
+        Me.Label1.Location = New System.Drawing.Point(52, 304)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(264, 29)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "El que tuvo más votos"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.0!)
+        Me.Label2.Location = New System.Drawing.Point(496, 304)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(292, 29)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "El que tuvo menos votos"
+        '
+        'ButtonGuardado
+        '
+        Me.ButtonGuardado.Location = New System.Drawing.Point(485, 129)
+        Me.ButtonGuardado.Name = "ButtonGuardado"
+        Me.ButtonGuardado.Size = New System.Drawing.Size(193, 100)
+        Me.ButtonGuardado.TabIndex = 5
+        Me.ButtonGuardado.Text = "CSV ya guardado"
+        Me.ButtonGuardado.UseVisualStyleBackColor = True
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!)
+        Me.Label3.Location = New System.Drawing.Point(95, 34)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(596, 25)
+        Me.Label3.TabIndex = 6
+        Me.Label3.Text = "Toque cualquiera de los 2 botones para realizar un calculo de votos"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!)
+        Me.Label4.Location = New System.Drawing.Point(62, 90)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(290, 25)
+        Me.Label4.TabIndex = 7
+        Me.Label4.Text = "Buscar el archivo CSV en tu PC"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!)
+        Me.Label5.Location = New System.Drawing.Point(414, 90)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(319, 25)
+        Me.Label5.TabIndex = 8
+        Me.Label5.Text = "Utilizar el CSV de votos ya cargado"
+        '
+        'FrmProb6
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.ButtonGuardado)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBoxMenos)
+        Me.Controls.Add(Me.TextBoxMas)
+        Me.Controls.Add(Me.ButtonExplorar)
+        Me.Name = "FrmProb6"
         Me.Text = "FrmProb6"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
+
+    Friend WithEvents ButtonExplorar As Button
+    Friend WithEvents TextBoxMas As TextBox
+    Friend WithEvents TextBoxMenos As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents ButtonGuardado As Button
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
 End Class
---
+++ clases/CSVReader.vb
@@ -0,0 +1,31 @@
+﻿Imports Microsoft.VisualBasic.FileIO
+Public Class CSVReader
+
+    Private CSVruta As String
+    Private delimitador As String
+
+    Public Sub New(CSVruta As String, Optional delimitador As String = ";")
+        Me.CSVruta = CSVruta
+        Me.delimitador = delimitador
+    End Sub
+
+    Public Function cargarDatosCSV() As String()()
+        Try
+            Dim datos As New List(Of String())()
+
+            Using parser As New TextFieldParser(CSVruta)
+                parser.TextFieldType = FieldType.Delimited
+                parser.SetDelimiters(Me.delimitador)
+
+                While Not parser.EndOfData
+                    datos.Add(parser.ReadFields())
+                End While
+            End Using
+
+            Return datos.ToArray()
+        Catch ex As Exception
+            Throw New Exception("Error al cargar el archivo CSV: " & ex.Message)
+        End Try
+    End Function
+
+End Class

683ebc945c8408f155847c31aede85c047c2cd97
Alik <moisesalik01@gmail.com>
2023-10-15 16:28:50 -0500

    problema 5 terminado

--- FrmProb5.Designer.vb
+++ FrmProb5.Designer.vb
@@ -22,9 +22,65 @@ Partial Class FrmProb5
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TextBoxCadena = New System.Windows.Forms.TextBox()
+        Me.ListBoxResultados = New System.Windows.Forms.ListBox()
+        Me.ButtonEjecutar = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!)
+        Me.Label1.Location = New System.Drawing.Point(146, 39)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(167, 25)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Ingrese la cadena"
+        '
+        'TextBoxCadena
+        '
+        Me.TextBoxCadena.Location = New System.Drawing.Point(330, 42)
+        Me.TextBoxCadena.Name = "TextBoxCadena"
+        Me.TextBoxCadena.Size = New System.Drawing.Size(323, 22)
+        Me.TextBoxCadena.TabIndex = 1
+        '
+        'ListBoxResultados
+        '
+        Me.ListBoxResultados.FormattingEnabled = True
+        Me.ListBoxResultados.ItemHeight = 16
+        Me.ListBoxResultados.Location = New System.Drawing.Point(151, 87)
+        Me.ListBoxResultados.Name = "ListBoxResultados"
+        Me.ListBoxResultados.Size = New System.Drawing.Size(502, 244)
+        Me.ListBoxResultados.TabIndex = 2
+        '
+        'ButtonEjecutar
+        '
+        Me.ButtonEjecutar.Location = New System.Drawing.Point(308, 361)
+        Me.ButtonEjecutar.Name = "ButtonEjecutar"
+        Me.ButtonEjecutar.Size = New System.Drawing.Size(161, 62)
+        Me.ButtonEjecutar.TabIndex = 3
+        Me.ButtonEjecutar.Text = "Ejecutar algoritmo"
+        Me.ButtonEjecutar.UseVisualStyleBackColor = True
+        '
+        'FrmProb5
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.ButtonEjecutar)
+        Me.Controls.Add(Me.ListBoxResultados)
+        Me.Controls.Add(Me.TextBoxCadena)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "FrmProb5"
         Me.Text = "FrmProb5"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TextBoxCadena As TextBox
+    Friend WithEvents ListBoxResultados As ListBox
+    Friend WithEvents ButtonEjecutar As Button
 End Class

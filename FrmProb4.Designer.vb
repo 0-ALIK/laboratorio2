575e4997f1a2a7f825146525fbf680ccec8435ba
AnghelKupo <shawtybrave.lexus@gmail.com>
2023-10-15 21:25:58 -0500

    Pantalla embellezida

--- FrmProb4.Designer.vb
+++ FrmProb4.Designer.vb
@@ -26,52 +26,102 @@ Partial Class FrmProb4
         Me.Label1 = New System.Windows.Forms.Label()
         Me.txtBoxR = New System.Windows.Forms.TextBox()
         Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Panel1.SuspendLayout()
         Me.SuspendLayout()
         '
         'txtBoxAno
         '
-        Me.txtBoxAno.Location = New System.Drawing.Point(238, 83)
+        Me.txtBoxAno.BackColor = System.Drawing.Color.Sienna
+        Me.txtBoxAno.Font = New System.Drawing.Font("Century Gothic", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtBoxAno.Location = New System.Drawing.Point(24, 77)
+        Me.txtBoxAno.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
         Me.txtBoxAno.Name = "txtBoxAno"
-        Me.txtBoxAno.Size = New System.Drawing.Size(100, 20)
+        Me.txtBoxAno.Size = New System.Drawing.Size(135, 31)
         Me.txtBoxAno.TabIndex = 0
+        Me.txtBoxAno.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
         '
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(149, 86)
+        Me.Label1.Font = New System.Drawing.Font("Century Gothic", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(20, 25)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(74, 13)
+        Me.Label1.Size = New System.Drawing.Size(140, 22)
         Me.Label1.TabIndex = 1
         Me.Label1.Text = "Ingresa el año"
         '
         'txtBoxR
         '
-        Me.txtBoxR.Location = New System.Drawing.Point(515, 79)
+        Me.txtBoxR.BackColor = System.Drawing.Color.Sienna
+        Me.txtBoxR.Cursor = System.Windows.Forms.Cursors.No
+        Me.txtBoxR.Font = New System.Drawing.Font("Century Gothic", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtBoxR.Location = New System.Drawing.Point(224, 77)
+        Me.txtBoxR.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.txtBoxR.Multiline = True
         Me.txtBoxR.Name = "txtBoxR"
         Me.txtBoxR.ReadOnly = True
-        Me.txtBoxR.Size = New System.Drawing.Size(229, 20)
+        Me.txtBoxR.Size = New System.Drawing.Size(300, 70)
         Me.txtBoxR.TabIndex = 3
+        Me.txtBoxR.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(361, 233)
+        Me.Button1.BackColor = System.Drawing.Color.Sienna
+        Me.Button1.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.Button1.Font = New System.Drawing.Font("Century Gothic", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(24, 133)
+        Me.Button1.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
         Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.Size = New System.Drawing.Size(135, 46)
         Me.Button1.TabIndex = 4
         Me.Button1.Text = "Calcular"
-        Me.Button1.UseVisualStyleBackColor = True
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.BackColor = System.Drawing.Color.Transparent
+        Me.Label2.Font = New System.Drawing.Font("Century Gothic", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(264, 28)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(295, 44)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "BIENVENIDO" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Fechas de domingo de Pascua"
+        Me.Label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.Color.Transparent
+        Me.Panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Panel1.Controls.Add(Me.txtBoxR)
+        Me.Panel1.Controls.Add(Me.Button1)
+        Me.Panel1.Controls.Add(Me.txtBoxAno)
+        Me.Panel1.Controls.Add(Me.Label1)
+        Me.Panel1.Location = New System.Drawing.Point(148, 125)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(556, 348)
+        Me.Panel1.TabIndex = 6
         '
         'FrmProb4
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Controls.Add(Me.Button1)
-        Me.Controls.Add(Me.txtBoxR)
-        Me.Controls.Add(Me.Label1)
-        Me.Controls.Add(Me.txtBoxAno)
+        Me.BackgroundImage = Global.laboratorio2.My.Resources.Resources.mitchell_luo_xREzOtWzDrQ_unsplash__1_
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
+        Me.ClientSize = New System.Drawing.Size(771, 511)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Panel1)
+        Me.Font = New System.Drawing.Font("Century Gothic", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
         Me.Name = "FrmProb4"
         Me.Text = "FrmProb4"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -81,4 +131,6 @@ Partial Class FrmProb4
     Friend WithEvents Label1 As Label
     Friend WithEvents txtBoxR As TextBox
     Friend WithEvents Button1 As Button
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Panel1 As Panel
 End Class

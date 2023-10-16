a1986a45e673dfa27679d7433f29e6f8ae61edd4
Alik <moisesalik01@gmail.com>
2023-10-15 23:37:10 -0500

    xd

--- My Project/Application.Designer.vb
+++ My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.laboratorio2.FrmProb2
+            Me.MainForm = Global.laboratorio2.FrmPrincipal
         End Sub
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _

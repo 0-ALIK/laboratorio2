0d41acf72e544b2c27486ff03ba04e17f32c2284
Martes_de_sandia <alyson.torres@utp.ac.pa>
2023-10-15 19:24:28 -0500

    aqui esta mi pascua :P

--- FrmProb4.Designer.vb
+++ FrmProb4.Designer.vb
@@ -22,9 +22,63 @@ Partial Class FrmProb4
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container
+        Me.txtBoxAno = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtBoxR = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'txtBoxAno
+        '
+        Me.txtBoxAno.Location = New System.Drawing.Point(238, 83)
+        Me.txtBoxAno.Name = "txtBoxAno"
+        Me.txtBoxAno.Size = New System.Drawing.Size(100, 20)
+        Me.txtBoxAno.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(149, 86)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(74, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Ingresa el año"
+        '
+        'txtBoxR
+        '
+        Me.txtBoxR.Location = New System.Drawing.Point(515, 79)
+        Me.txtBoxR.Name = "txtBoxR"
+        Me.txtBoxR.ReadOnly = True
+        Me.txtBoxR.Size = New System.Drawing.Size(229, 20)
+        Me.txtBoxR.TabIndex = 3
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(361, 233)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 4
+        Me.Button1.Text = "Calcular"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'FrmProb4
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.txtBoxR)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtBoxAno)
+        Me.Name = "FrmProb4"
         Me.Text = "FrmProb4"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
+
+    Friend WithEvents txtBoxAno As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtBoxR As TextBox
+    Friend WithEvents Button1 As Button
 End Class
--- FrmProb4.vb
+++ FrmProb4.vb
@@ -1,3 +1,24 @@
 ﻿Public Class FrmProb4
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim ano As Integer = Val(txtBoxAno.Text)
+        Dim mes, dia As String
+        Dim A, B, C, D, _E, N As Integer
+        A = ano Mod 19
+        B = ano Mod 4
+        C = ano Mod 7
+        D = (19 * A + 24) Mod 30
+        _E = (2 * B + 4 * C + 6 * D + 5) Mod 7
+        N = 22 + D + _E
 
+        If N <= 31 Then
+            mes = "marzo"
+            dia = N
+        Else
+            mes = "abril"
+            dia = N - 31
+        End If
+
+        txtBoxR.Text = "Domingo de Pascua en " & ano & ": " & dia & " del " & mes
+
+    End Sub
 End Class
